06baa0b57cb4093f730d4a28864dc241aeea1d75
HuangMin <hmok@163.com>
2024-07-24 18:37:44 +0800

    删除开发sample,准备使用link方式映射

--- Assets/Samples/HMFWFairyGUIExtension/Scripts/UI/FguiSample.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HMFW
-{
-    [FGUIResUrl("FguiSample", "UIHome", "Assets/HMFWSampleBundle/Fgui/MainScene/UIHome_fui.bytes", "FguiSample",
-        new[] { "Assets/HMFWSampleBundle/Fgui/Common/UICommon_fui.bytes" }, true)]
-    public class FguiSample : FairyGUIBase
-    {
-    }
-}
--- Assets/Samples/SampleURP/UI/UISamplePlayerCoinInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HMFW.SampleURP.UI
-{
-    [HMFW.Core.UGUIRes("Assets/HMFWSampleBundle/UI/UISamplePlayerCoinInfo.prefab", "PlayerCoin", false)]
-    public class UISamplePlayerCoinInfo: HMFW.Core.UIBase
-    {
-        
-        
-    }
-}
